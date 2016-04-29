2890acba1d078c25661bb8fe55db93df7363602c
abdi <ahamada.abdillah06@gmail.com>
2016-04-29 16:48:35 +0200

    et de la couleur pour jordy

---
+++ compilateur/color_constantes.h
@@ -0,0 +1,19 @@
+#ifndef __COLOR_CONSTANTES___
+#define __COLOR_CONSTANTES__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#endif 
